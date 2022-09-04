7fd3f3baa9f5c747485064b9931af6821fa5db23
dudushy <eduardotalarico1337@gmail.com>
2022-09-04 18:45:33 -0300

    Add GET + SET

--- src/app/services/global-variables.service.ts
+++ src/app/services/global-variables.service.ts
@@ -1,11 +1,41 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GlobalVariablesService {
+  variables: any;
 
   constructor( ) {
     console.log('[GlobalVariablesService#constructor]');
+
+    this.variables = [];
+
+    this.setVar('test', [
+      'test',
+      'create',
+      'read',
+      'update',
+      'delete'
+    ]);
+
+    console.log('[GlobalVariablesService#constructor] variables', this.variables);
+  }
+
+  getVar(varname: any) {
+    // console.log(`[GlobalVariablesService#getVar] variables[${varname}]`, this.variables[varname]);
+
+    if (this.variables[varname] != undefined) {
+      return this.variables[varname];
+    } else {
+      return null;
+    }
+  }
+
+  setVar(varname: any, value: any) {
+    this.variables[varname] = value;
+
+    // console.log(`[GlobalVariablesService#setVar] variables[${varname}]`, this.variables[varname]);
   }
 }
