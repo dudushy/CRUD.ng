1f9316236a89520b44a0f7e700fbc0d352b80d1b
dudushy <eduardotalarico1337@gmail.com>
2022-09-05 19:17:33 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 BASE_URL='https://dudushy.github.io/CRUD.ng/'
 PROJECT_NAME='CRUD'
+COMMIT_MESSAGE='build.sh'
 
 ng build --base-href $BASE_URL
-npx angular-cli-ghpages --dir=dist/$PROJECT_NAME --message='build.sh'
+npx angular-cli-ghpages --dir=dist/$PROJECT_NAME --message=$COMMIT_MESSAGE
