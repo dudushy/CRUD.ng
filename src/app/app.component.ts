f196a11424b91b49cdfe7a261a433eb5399bfb24
dudushy <eduardotalarico1337@gmail.com>
2022-09-05 18:28:36 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,20 +11,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'CRUD';
+  url = '';
 
   constructor(
     public GVS: GlobalVariablesService,
     private router: Router
   ) {
     console.log('[AppComponent#constructor]');
+    console.log('[AppComponent#constructor] (GVS) all_pages', this.GVS.getVar('all_pages'));
 
-    console.log('[AppComponent#constructor] (GVS) test', this.GVS.getVar('test'));
+    this.updateURL();
+  }
+
+  updateURL() {
+    const newUrl = this.router.url;
+    console.log('[AppComponent#updateURL] newUrl', newUrl);
+
+    if (newUrl == '/') {
+      this.url = this.GVS.getVar('base_url');
+    } else {
+      this.url = newUrl;
+    }
+
+    console.log('[AppComponent#updateURL] url', this.url);
   }
 
   redirectTo(url: any) {
     console.log('[AppComponent#redirectTo] url', url);
     this.router.navigateByUrl(`/${url}`);
-    // this.router.navigateByUrl('/' + url);
+    this.updateURL();
   }
 }
