48ff83e3f4fbf1be1dd8371c2a177995531ca72c
dudushy <eduardotalarico1337@gmail.com>
2022-09-07 14:45:08 -0300

    Not done yet, `getFbDoc`

--- src/app/services/global-variables.service.ts
+++ src/app/services/global-variables.service.ts
@@ -1,19 +1,43 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Injectable } from '@angular/core';
 
+import { SECRETS } from '../../assets/secrets/secrets';
+
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+import { getFirestore, doc, collection, addDoc, getDocs, setDoc } from 'firebase/firestore';
+
 @Injectable({
   providedIn: 'root'
 })
 export class GlobalVariablesService {
   variables: any = [];
 
+  fbApp = initializeApp(SECRETS.firebaseConfig);
+  fbAnalytics = getAnalytics(this.fbApp);
+  db = getFirestore(this.fbApp);
+
   constructor() {
     console.log('[GlobalVariablesService#constructor]');
 
     this.setupVars();
 
-    this.variables.theme = 'dark';
+    this.test();
+  }
 
+  async test() {
+    this.addFbDoc('test', 'dudu', 20);
+    console.log('[GlobalVariablesService#constructor] getFbDocs (1)');
+    await this.getFbDocs('test');
+    console.log('[GlobalVariablesService#constructor] getFbDocs (2)');
+    await this.getFbDocs('test');
+  }
+
+  setupVars() {
+    console.log('[GlobalVariablesService#setupVars]');
+
+    this.variables.theme = 'dark';
     this.variables.all_pages = [
       'test',
       'create',
@@ -21,18 +45,11 @@ export class GlobalVariablesService {
       'update',
       'delete'
     ];
-
     this.variables.base_url = 'test';
-
     this.variables.current_url = 'test';
+    console.log('[GlobalVariablesService#setupVars] variables', this.variables);
 
-    console.log('[GlobalVariablesService#constructor] variables', this.variables);
-  }
-
-  setupVars() {
-    console.log('[GlobalVariablesService#setupVars]');
     this.setVar('theme', 'dark');
-
     this.setVar('all_pages', [
       'test',
       'create',
@@ -40,9 +57,7 @@ export class GlobalVariablesService {
       'update',
       'delete'
     ]);
-
     this.setVar('base_url', 'test');
-
     this.setVar('current_url', 'test');
   }
 
@@ -60,5 +75,46 @@ export class GlobalVariablesService {
     // this.variables[varname] = value;
 
     localStorage.setItem(varname, JSON.stringify(value));
+  }
+
+  async setFbDoc(table: any, id: any, varname: any, value: any) {
+    const params = {
+      table: table,
+      id: id,
+      varname: varname,
+      value: value
+    };
+    console.log('[GlobalVariablesService#setFbDoc] params', params);
+
+    await setDoc(
+      doc(this.db, table, id),
+      {
+        [varname]: value
+      },
+      {
+        merge: true
+      }
+    );
+  }
+
+  async addFbDoc(table: any, varname: any, value: any) {
+    try {
+      const docRef = await addDoc(collection(this.db, table), {
+        [varname]: value
+      });
+      // console.log('[GlobalVariablesService#addFbDoc] docRef', docRef);
+      console.log(`[GlobalVariablesService#addFbDoc] ${table}/${varname} success | id`, docRef.id);
+    } catch (e) {
+      console.error(`[GlobalVariablesService#addFbDoc] ${table}/${varname} error`, e);
+    }
+  }
+
+  async getFbDocs(table: any) {
+    const querySnapshot = await getDocs(collection(this.db, table));
+    // console.log('[GlobalVariablesService#getFbDocs] querySnapshot', querySnapshot);
+    querySnapshot.forEach((doc) => {
+      console.log(`[GlobalVariablesService#getFbDocs] ${doc.id}`, doc.data());
+      this.setFbDoc(table, doc.id, 'dudushy', 'natan');
+    });
   }
 }
