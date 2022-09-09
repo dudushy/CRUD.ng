305b2b8bcaa52623e3b24a36a094ab7cc6a2449e
dudushy <eduardotalarico1337@gmail.com>
2022-09-09 19:40:24 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,26 @@
-PAGE_URL='https://dudushy.github.io/CRUD.ng/'
-PROJECT_NAME='CRUD'
-COMMIT_MESSAGE='`build.sh`'
+#! title preset
+TITLE='[build.sh]'
+echo $TITLE 'start'
 
+echo $TITLE 'setup config...'
+echo $TITLE '- - - - - - - - - - - - - - - - - - - - - - - - - -'
+#* vars
+
+PAGE_URL='https://dudushy.github.io/CRUD.ng/'
+echo $TITLE '- PAGE_URL=' $PAGE_URL
+
+PROJECT_NAME='CRUD'
+echo $TITLE '- PROJECT_NAME=' $PROJECT_NAME
+
+COMMIT_MESSAGE='`build.sh`'
+echo $TITLE '- COMMIT_MESSAGE=' $COMMIT_MESSAGE
+
+echo $TITLE '- - - - - - - - - - - - - - - - - - - - - - - - - -'
+echo $TITLE 'setup config done!'
+
+#* main
+echo $TITLE 'run'
 ng build --base-href $PAGE_URL
 npx angular-cli-ghpages --dir=dist/$PROJECT_NAME --message=$COMMIT_MESSAGE
+
+echo $TITLE 'finished.'
