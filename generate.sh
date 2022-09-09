13acbc9b46d853beef7205c0d7dd8402bcccb150
dudushy <eduardotalarico1337@gmail.com>
2022-09-09 19:40:21 -0300

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,3 +1,9 @@
+#! title preset
+TITLE='[generate.sh]'
+echo $TITLE 'start'
+
+#* main
+echo $TITLE 'run'
 ng generate component test
 ng generate component login
 ng generate component create
@@ -5,3 +11,5 @@ ng generate component read
 ng generate component update
 ng generate component delete
 ng generate service services/global-variables
+
+echo $TITLE 'finished.'
