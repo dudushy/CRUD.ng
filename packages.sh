d8a90b5c2a1a79ec6bd4e68880cc422619e10c82
dudushy <eduardotalarico1337@gmail.com>
2022-09-09 22:39:16 -0300

    `packages.sh`

--- packages.sh
+++ packages.sh
@@ -1,12 +1,11 @@
 #! title preset
 TITLE='[packages.sh]'
-echo $TITLE 'start'
 
 #* main
 echo $TITLE 'run'
-npm i @angular/cli
-npm i eslint
-npm i angular-cli-ghpages
-npm i firebase
+npm i @angular/cli@14.1.2
+npm i eslint@8.23.0
+npm i angular-cli-ghpages@1.0.3
+npm i firebase@9.9.4
 
 echo $TITLE 'finished.'
